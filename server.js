a951fa21d8398878c6931f4f76b57582ad9e3d9f
kokosevi <s.sager91@gmail.com>
2022-08-25 15:20:00 +0200

    test

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
-const path = require('path')
-const http = require("http")
-const fs = require("fs")
+const path = require('path');
+const http = require("http");
+const fs = require("fs");
 // const mime = require('mime');
-const express = require("express")
+//const express = require("express")
 
-const app = express(); 
-const port = 3000
+//const app = express(); 
+const port = 3000;
 
-const server = http.createServer(function(req, res){
-    res.writeHead(200, { "Content-Type": "text/html" })
-    fs.readFile("index.html", function(error, data){
+const server = http.createServer((req, res) => {
+    // for '/' get index.html otherwise strip initial slash character from file path
+    const url = req.url === "/" ? "index.html" : req.url.substring(1); 
+
+    // prepare content type depending on file extension
+    const ext = url.split(".")[1].toLowerCase();
+    let contentType = "text/html";
+    switch (ext){
+        case "css":
+            contentType = "text/css";
+            break;
+    }
+
+    // create resonse headers
+    res.writeHead(200, { "Content-Type": contentType });
+
+    // return file to client
+    fs.readFile(url, (error, data) => {
         if(error){
-            res.writeHead(404)
-            res.write("File Not Found")
+            res.writeHead(404);
+            res.write("File Not Found");
         } else {
-            res.write(data)
+            res.write(data);
         }
-        res.end()
-    })
-    
-
-})
+        res.end();
+    });
+});
 
 
-server.listen(port, function(error){
+server.listen(port, error => {
     if(error){
         console.log("Something went wrong", error)
     } else{
         console.log("Server is listening on port " + port)
     }
-
-})
-
-
+});
